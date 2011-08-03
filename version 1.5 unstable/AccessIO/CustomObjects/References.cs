225c8096a3cf6c09d103183e3e13ec0eaff51182
mnieto <m.nieto@kevull.com>
2011-08-03 00:23:56 +0000

    Load references. Added Major and Minor properties when saving references to file.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS24]$/accesssvn;C7833

--- version 1.5 unstable/AccessIO/CustomObjects/References.cs
+++ version 1.5 unstable/AccessIO/CustomObjects/References.cs
@@ -31,16 +31,84 @@ namespace AccessIO {
                     if (!reference.BuiltIn && !reference.IsBroken) {
                         export.WriteBegin("Reference", reference.Name);
                         export.WriteProperty("FullPath", reference.FullPath);
-                        export.WriteProperty("Guid", reference.Guid);
-                        export.WriteEnd();
+                        if (reference.Kind == Microsoft.Vbe.Interop.vbext_RefKind.vbext_rk_TypeLib) {
+                            export.WriteProperty("Guid", reference.Guid);
+                            export.WriteProperty("Major", reference.Major);
+                            export.WriteProperty("Minor", reference.Minor);
+                            export.WriteEnd();
+                        }
                     }
                 }
                 export.WriteEnd();
             }
         }
 
+        /// <summary>
+        /// Ovewrite the current references with the references from <paramref name="fileName"/>
+        /// </summary>
+        /// <param name="fileName">Name of the file with the references to load into the database</param>
         public override void Load(string fileName) {
-            throw new NotImplementedException();
+            using (StreamReader sr = new StreamReader(fileName)) {
+                ImportObject import = new ImportObject(sr);
+
+                //Remove any previous reference
+                Access.Reference[] referencesList = new Access.Reference[App.Application.References.Count];
+                int i = 0;
+                foreach (Access.Reference reference in App.Application.References) {
+                    if (!reference.BuiltIn && !reference.IsBroken)
+                        referencesList[i++] = reference;
+                }
+                for (int j = 0; j < i; j++) {
+                    App.Application.References.Remove(referencesList[j]);
+                }
+
+                //Add new references from file
+                //if reference is dll or typelib, it will have guid and version
+                //if reference is an access file (mde,mdb,mda,adp,...) we need add the reference by filename
+                import.ReadLine();
+                import.ReadLine();  //Read the Begin Reference (or End References if there is no references)
+                while (!import.IsEnd) {
+                    import.ReadLine();  //Read FullPath
+                    string fullPath = import.PropertyValue();
+                    import.ReadLine();  //Read Gui or End Reference
+                    if (import.IsEnd) {
+                        fullPath = ResolveFileName(fullPath);
+                        App.Application.References.AddFromFile(fullPath);
+                    }  else {
+                        string guid = import.PropertyValue();
+                        import.ReadLine();  //Read Major
+                        int major = Convert.ToInt32(import.PropertyValue());
+                        import.ReadLine();  //Read Minor
+                        int minor = Convert.ToInt32(import.PropertyValue());
+                        import.ReadLine();  //Read End Reference
+                        App.Application.References.AddFromGuid(guid, major, minor);
+                        import.ReadLine();  //Read the next Begin Reference or End References
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Try to resolve the location of the filename
+        /// </summary>
+        /// <param name="fullPath">path of the file to find</param>
+        /// <returns>
+        /// if exists <paramref name="fullPath"/>, returns fullPath, else, 
+        /// it will find it in the same folder than the access file
+        /// If not found, return fullPath
+        /// </returns>
+        private string ResolveFileName(string fullPath) {
+            if (System.IO.File.Exists(fullPath))
+                return fullPath;
+            else {
+                string fileName = System.IO.Path.GetFileName(fullPath);
+                string folder = System.IO.Path.GetDirectoryName(App.FileName);
+                string newFullPath = System.IO.Path.Combine(folder, fileName);
+                if (System.IO.File.Exists(newFullPath))
+                    return newFullPath;
+                else
+                    return fullPath;
+            }
         }
 
         public override object this[string propertyName] {
